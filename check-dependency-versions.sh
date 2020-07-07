ea91e7296cff07a19416a859229cb3e1ae0b4143
Niels Basjes <niels@basjes.nl>
2020-07-07 18:21:03 +0200

    WIP

--- check-dependency-versions.sh
+++ check-dependency-versions.sh
@@ -5,6 +5,8 @@ echo "==============================="
 echo "===- CHECKING DEPENDENCIES -==="
 echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
 mvn versions:display-dependency-updates versions:display-plugin-updates | grep -E '( -> | Building )'
+mvn -Ddependency-check.skip=false dependency-check:check
+
 echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
 echo "===-         DONE!         -==="
 echo "==============================="
